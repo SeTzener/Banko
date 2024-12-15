77fd9fbf6018431c4600266528a159fd3bd35f08
setzener <gaetanovf@gmail.com>
2024-12-15 01:21:43 +0100

    CircularIndicator component logic

--- composeApp/src/commonMain/kotlin/com/banko/app/ui/components/CircularIndicator.kt
+++ composeApp/src/commonMain/kotlin/com/banko/app/ui/components/CircularIndicator.kt
@@ -1,18 +1,22 @@
 package com.banko.app.ui.components
 
-import androidx.compose.animation.animateColorAsState
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.animateIntAsState
 import androidx.compose.animation.core.tween
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
@@ -30,184 +34,131 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
+import com.banko.app.Category
 
 @Composable
 fun CircularIndicator(
-    bigTextFontSize: TextUnit = MaterialTheme.typography.h2.fontSize,
-    bigTextSuffix: String,
-    backgroundIndicatorColor: Color = MaterialTheme.colors.onSurface.copy(alpha = 0.99f),
-    bigTextColor: Color = MaterialTheme.colors.onSurface, // forse questo lo posso cambiare per metterci i vari colori
-    canvasSize: Dp = 300.dp, // Da adattare alla misura che voglio realmente nello schermo
-    foregroundColor: Color = Color.Blue,
-    indicatorStroke: Float = 100f,
-    indicatorValue: Int = 0,
-    maxIndicatorValue: Int = 100,
-    smallText: String,
-    smallTextColor: Color = MaterialTheme.colors.onSurface.copy(alpha = 0.5f),
-    smallTextFontSize: TextUnit = MaterialTheme.typography.body1.fontSize,
+    currency: String,
+    monthlyBudget: Int = 0,
+    dailyBudgetText: String,
+    monthlyBudgetText: String,
+    categories: List<Category>,
+    bigTextFontSize: TextUnit = MaterialTheme.typography.titleLarge.fontSize,
+    bigTextColor: Color = MaterialTheme.colorScheme.onBackground,
+    canvasSize: Dp = 300.dp,
+    indicatorStroke: Float = 60f,
+    dailyBudget: Int = 0,
+    smallTextColor: Color = MaterialTheme.colorScheme.onBackground.copy(alpha = 0.5f),
+    smallTextFontSize: TextUnit = MaterialTheme.typography.bodyMedium.fontSize,
 ) {
-    var allowedIndicatorValue by remember {
-        mutableIntStateOf(maxIndicatorValue)
-    }
-    // TODO(): Questo blocco non serve
-    // ----------------------------------------------------
-    allowedIndicatorValue = if (indicatorValue <= maxIndicatorValue) {
-        indicatorValue
-    } else {
-        maxIndicatorValue
-    }
-    // ----------------------------------------------------
-    var animatedIndicatorValue by remember {
-        mutableFloatStateOf(0f)
-    }
-    LaunchedEffect(key1 = allowedIndicatorValue) {
-        animatedIndicatorValue = allowedIndicatorValue.toFloat()
-    }
-    val percentage = (animatedIndicatorValue / maxIndicatorValue) * 100
+    val totalAmount = categories.sumOf { it.amount.toInt() }.toFloat()
+    var animatedMonthlyBudgetValue by remember { mutableIntStateOf(0) }
+    var animatedDailyBudgetValue by remember { mutableIntStateOf(0) }
 
-    // The arc begins at startAngle and spans sweepAngle degrees counterclockwise.
-    // A value of 360 degrees would draw a full circle.
-    val sweepAngle by animateFloatAsState(
-        targetValue = (2.4 * percentage).toFloat(),
-        animationSpec = tween(1000),
-        label = ""
-    )
-    val receivedValue by animateIntAsState(
-        targetValue = allowedIndicatorValue,
-        animationSpec = tween(1000),
-        label = ""
-    )
-    // TODO(): che testo colora sto coso?
-    val animatedBigTextColor by animateColorAsState(
-        targetValue = if (allowedIndicatorValue == 0) {
-            MaterialTheme.colors.onSurface.copy(alpha = 0.3f)
-        } else {
-            bigTextColor
-        },
+    LaunchedEffect(dailyBudget) {
+        animatedMonthlyBudgetValue = monthlyBudget
+    }
+    LaunchedEffect(dailyBudget) {
+        animatedDailyBudgetValue = dailyBudget
+    }
+
+    val animatedDailyBudget by animateIntAsState(
+        targetValue = animatedDailyBudgetValue,
         animationSpec = tween(1000),
         label = ""
     )
 
+    val animatedMonthlyBudget by animateIntAsState(
+        targetValue = animatedMonthlyBudgetValue,
+        animationSpec = tween(1000),
+        label = ""
+    )
+    // Remember and animate the sweep angles for each category
+    val animatedSweepAngles = categories.mapIndexed { index, category ->
+        animateFloatAsState(
+            targetValue = (category.amount.toInt() / totalAmount) * 240f,
+            animationSpec = tween(1000),
+            label = ""
+        ).value
+    }
     Column(
-        modifier = Modifier
-            .size(canvasSize)
-            .drawBehind {
-                val componentSize = size / 1.25f
-                backgroundIndicator(
-                    componentSize = componentSize,
-                    indicatorColor = backgroundIndicatorColor,
-                    indicatorStrokeWidth = indicatorStroke
-                )
-                foregroundIndicator(
-                    startAngle = 150f, // È il punto da cui parte il tag
-                    sweepAngle = sweepAngle,
-                    componentSize = componentSize,
-                    indicatorColor = foregroundColor,
-                    indicatorStrokeWidth = indicatorStroke
-                )
-            },
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        EmbeddedElements(
-            bigText = receivedValue,
-            bigTextFontSize = bigTextFontSize,
-            bigTextColor = animatedBigTextColor,
-            bigTextSuffix = bigTextSuffix,
-            smallText = smallText,
-            smallTextColor = smallTextColor,
-            smallTextFontSize = smallTextFontSize,
-        )
+        Column(
+            modifier = Modifier
+                .size(canvasSize)
+                .drawBehind {
+                    val componentSize = size / 1.25f
+                    drawForegroundIndicator(
+                        categories = categories,
+                        animatedSweepAngles = animatedSweepAngles,
+                        startAngle = 150f, // È il punto da cui parte il tag
+                        componentSize = componentSize,
+                        indicatorStroke = indicatorStroke,
+                    )
+                },
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            EmbeddedElements(
+                dailyBudget = animatedDailyBudget,
+                monthlyBudget = animatedMonthlyBudget,
+                bigTextFontSize = bigTextFontSize,
+                bigTextColor = bigTextColor,
+                currency = currency,
+                dailyBudgetText = dailyBudgetText,
+                monthlyBudgetText = monthlyBudgetText,
+                smallTextColor = smallTextColor,
+                smallTextFontSize = smallTextFontSize,
+            )
+        }
+        CategoryGrid(categories = categories)
     }
 }
 
-/**
- * Draws the shape of the indicator's foreground as an arc.
- *
- * This function uses the given size, color, and stroke width to draw a rounded arc
- * that serves as the background for an indicator component. The arc is drawn on the
- * [DrawScope] at a fixed angle and position based on the component size.
- *
- * @param componentSize The size of the indicator background to be drawn.
- * @param indicatorColor The color of the indicator's background.
- * @param indicatorStrokeWidth The stroke width used to draw the indicator's background.
- */
-private fun DrawScope.foregroundIndicator(
+fun DrawScope.drawForegroundIndicator(
+    categories: List<Category>,
+    animatedSweepAngles: List<Float>,
+    componentSize: Size,
+    indicatorStroke: Float,
     startAngle: Float,
-    sweepAngle: Float,
-    componentSize: Size,
-    indicatorColor: Color,
-    indicatorStrokeWidth: Float
 ) {
-    drawArc(
-        size = componentSize,
-        color = indicatorColor,
-        // this is the point where the arc starts.
-        startAngle = startAngle,
-        // The arc begins at startAngle and spans sweepAngle degrees counterclockwise.
-        // A value of 360 degrees would draw a full circle.
-        sweepAngle = sweepAngle,
-        useCenter = false,
-        style = Stroke(
-            width = indicatorStrokeWidth,
-            cap = StrokeCap.Round
-        ),
-        topLeft = Offset(
-            x = (size.width - componentSize.width) / 2f,
-            y = (size.height - componentSize.height) / 2f,
+    var currentStartAngle = startAngle
+    categories.forEachIndexed { index, category ->
+        drawArc(
+            size = componentSize,
+            color = category.color,
+            startAngle = currentStartAngle,
+            sweepAngle = animatedSweepAngles[index],
+            useCenter = false,
+            style = Stroke(
+                width = indicatorStroke,
+                cap = StrokeCap.Square
+            ),
+            topLeft = Offset(
+                (size.width - componentSize.width) / 2f,
+                (size.height - componentSize.height) / 2f
+            )
         )
-    )
-
-}
-
-/**
- * Draws the shape of the indicator's background as an arc.
- *
- * This function uses the given size, color, and stroke width to draw a rounded arc
- * that serves as the background for an indicator component. The arc is drawn on the
- * [DrawScope] at a fixed angle and position based on the component size.
- *
- * @param componentSize The size of the indicator background to be drawn.
- * @param indicatorColor The color of the indicator's background.
- * @param indicatorStrokeWidth The stroke width used to draw the indicator's background.
- */
-private fun DrawScope.backgroundIndicator(
-    componentSize: Size,
-    indicatorColor: Color,
-    indicatorStrokeWidth: Float
-) {
-    drawArc(
-        size = componentSize,
-        color = indicatorColor,
-        // this is the point where the arc starts.
-        startAngle = 150f,
-        // The arc begins at startAngle and spans sweepAngle degrees counterclockwise.
-        // A value of 360 degrees would draw a full circle.
-        sweepAngle = 240f,
-        useCenter = false,
-        style = Stroke(
-            width = indicatorStrokeWidth,
-            cap = StrokeCap.Round
-        ),
-        topLeft = Offset(
-            x = (size.width - componentSize.width) / 2f,
-            y = (size.height - componentSize.height) / 2f,
-        )
-    )
+        currentStartAngle += animatedSweepAngles[index]
+    }
 }
 
 @Composable
-fun EmbeddedElements(
-    bigText: Int,
+private fun EmbeddedElements(
+    dailyBudget: Int,
+    monthlyBudget: Int,
     bigTextFontSize: TextUnit,
     bigTextColor: Color,
-    bigTextSuffix: String,
-    smallText: String,
+    currency: String,
+    monthlyBudgetText: String,
+    dailyBudgetText: String,
     smallTextColor: Color,
     smallTextFontSize: TextUnit
-){
+) {
     Text(
-        text = smallText,
+        text = monthlyBudgetText,
         color = smallTextColor,
         fontSize = smallTextFontSize,
         textAlign = TextAlign.Center,
@@ -215,10 +166,70 @@ fun EmbeddedElements(
     )
 
     Text(
-        text = "$bigText ${bigTextSuffix.take(2)}",
+        text = "$monthlyBudget $currency",
         color = bigTextColor,
         fontSize = bigTextFontSize,
         textAlign = TextAlign.Center,
         fontWeight = FontWeight.Bold
     )
+    Text(
+        text = dailyBudgetText,
+        color = smallTextColor,
+        fontSize = smallTextFontSize,
+        textAlign = TextAlign.Center,
+        fontWeight = FontWeight.Bold
+    )
+
+    Text(
+        text = "$dailyBudget $currency",
+        color = bigTextColor,
+        fontSize = bigTextFontSize,
+        textAlign = TextAlign.Center,
+        fontWeight = FontWeight.Bold
+    )
+}
+
+@Composable
+private fun CategoryGrid(categories: List<Category>, itemsPerRow: Int = 5) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth().offset(y = (-60).dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        // Break the list into chunks of `itemsPerRow`
+        categories.chunked(itemsPerRow).forEach { rowCategories ->
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceEvenly
+            ) {
+                rowCategories.forEach { category ->
+                    Column(
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        modifier = Modifier.padding(horizontal = 8.dp)
+                    ) {
+                        Box(
+                            modifier = Modifier
+                                .size(12.dp)
+                                .background(
+                                    color = category.color,
+                                    shape = MaterialTheme.shapes.small
+                                )
+                        )
+                        Text(
+                            text = category.amount.toString(),
+                            style = MaterialTheme.typography.bodySmall,
+                            color = MaterialTheme.colorScheme.onBackground,
+                        )
+                        Text(
+                            text = category.name,
+                            style = MaterialTheme.typography.bodySmall,
+                            color = MaterialTheme.colorScheme.onBackground.copy(alpha = 0.6f),
+                        )
+                    }
+                }
+            }
+        }
+    }
 }
