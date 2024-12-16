0f106318ac8943d7a238a8242e83bfb1ecd088f6
setzener <gaetanovf@gmail.com>
2024-12-16 23:30:59 +0100

    Android and iOS native entry points adaptations

--- composeApp/src/iosMain/kotlin/com/banko/app/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/com/banko/app/MainViewController.kt
@@ -1,5 +1,12 @@
 package com.banko.app
 
+import androidx.compose.runtime.remember
 import androidx.compose.ui.window.ComposeUIViewController
+import com.arkivanov.decompose.DefaultComponentContext
+import com.arkivanov.essenty.lifecycle.LifecycleRegistry
+import com.banko.app.ui.Screens.navigation.RootComponent
 
-fun MainViewController() = ComposeUIViewController { App() }
+fun MainViewController() = ComposeUIViewController {
+    val root = remember { RootComponent(DefaultComponentContext(LifecycleRegistry())) }
+    App(root)
+}
