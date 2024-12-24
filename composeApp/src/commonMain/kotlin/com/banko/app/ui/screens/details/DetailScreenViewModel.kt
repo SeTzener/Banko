eb3ad658b1d11b7220a79386932d36dafb1106e4
setzener <gaetanovf@gmail.com>
2024-12-24 04:16:16 +0100

    - Changes the DetailScreen to try the apiClient. - Adds the viewmodel - Adds the state

---
+++ composeApp/src/commonMain/kotlin/com/banko/app/ui/screens/details/DetailScreenViewModel.kt
@@ -0,0 +1,31 @@
+package com.banko.app.ui.screens.details
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.banko.app.api.HttpClientProvider
+import com.banko.app.api.services.NordigenApiService
+import kotlinx.coroutines.launch
+
+// TODO(): Temporary, change it to actual implementation
+class DetailScreenViewModel : ViewModel() {
+    private val apiService = NordigenApiService(HttpClientProvider.client)
+    var screenState: DetailScreenState by mutableStateOf(DetailScreenState())
+
+    init {
+        fetchExampleData()
+    }
+
+    private fun fetchExampleData() {
+        viewModelScope.launch {
+            try {
+                val data = apiService.getExampleData()
+                screenState = screenState.copy(data = data.fact)
+            } catch (e: Exception) {
+                println("Error fetching data: ${e.message}")
+            }
+        }
+    }
+}
