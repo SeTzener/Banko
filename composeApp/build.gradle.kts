7f1c9661e6b886ac3d99fe4e22ac424b9196e4ed
setzener <gaetanovf@gmail.com>
2024-12-29 05:10:29 +0100

    Adds libraries

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -57,6 +57,10 @@ kotlin {
             implementation(libs.ktor.client.serialization)
             implementation(libs.kotlinx.datetime)
             implementation(libs.kotlinx.serialization.json)
+            implementation(libs.kotlinx.serialization.core)
+
+            api(libs.datastore)
+            api(libs.datastore.preferences)
         }
     }
 }
