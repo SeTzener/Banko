392059960cc85f80b270e7bbde600b6b8fdf58cc
setzener <gaetanovf@gmail.com>
2024-12-30 06:56:59 +0100

    inizializes koin in ios

--- composeApp/src/iosMain/kotlin/com/banko/app/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/com/banko/app/MainViewController.kt
@@ -4,9 +4,14 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.window.ComposeUIViewController
 import com.arkivanov.decompose.DefaultComponentContext
 import com.arkivanov.essenty.lifecycle.LifecycleRegistry
+import com.banko.app.di.initKoin
 import com.banko.app.ui.screens.navigation.RootComponent
 
-fun MainViewController() = ComposeUIViewController {
+fun MainViewController() = ComposeUIViewController(
+    configure = {
+        initKoin()
+    }
+) {
     val root = remember { RootComponent(DefaultComponentContext(LifecycleRegistry())) }
     val preferences = remember { createDataStore() }
     App(
