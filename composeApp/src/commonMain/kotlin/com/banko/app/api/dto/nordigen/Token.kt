4730997b27581b4ef8715fc16b96045ceb3d6420
setzener <gaetanovf@gmail.com>
2025-02-05 02:43:09 +0100

    Fixing the Token object

--- composeApp/src/commonMain/kotlin/com/banko/app/api/dto/nordigen/Token.kt
+++ composeApp/src/commonMain/kotlin/com/banko/app/api/dto/nordigen/Token.kt
@@ -4,8 +4,8 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class Token(
-    val access: String,
-    val refresh: String,
-    val access_expires: Long,
-    val refresh_expires: Long
+    val access: String = "",
+    val refresh: String = "",
+    val access_expires: Long = 0,
+    val refresh_expires: Long = 0
 )
