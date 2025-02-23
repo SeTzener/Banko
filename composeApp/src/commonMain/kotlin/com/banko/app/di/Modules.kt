a8a400b53ebc8366f1123e5e6ab415d31a3ecd38
setzener <gaetanovf@gmail.com>
2025-02-23 12:48:25 +0100

    Dependencies through koin

--- composeApp/src/commonMain/kotlin/com/banko/app/di/Modules.kt
+++ composeApp/src/commonMain/kotlin/com/banko/app/di/Modules.kt
@@ -4,8 +4,11 @@ import com.banko.app.api.services.NordigenTokenProvider
 import com.banko.app.api.services.OauthNordigenApi
 import com.banko.app.api.services.TokenInterceptor
 import com.banko.app.database.BankoDatabase
-import com.banko.app.database.repository.BankoDatabaseRepository
+import com.banko.app.database.repository.TransactionsRepository
 import com.banko.app.database.CreateDatabase
+import com.banko.app.database.repository.ExpenseTagRepository
+import com.banko.app.ui.screens.settings.SettingsScreenViewModel
+import org.koin.compose.viewmodel.dsl.viewModelOf
 import org.koin.core.module.Module
 import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
@@ -18,7 +21,11 @@ val  sharedModule = module {
     singleOf(::NordigenTokenProvider)
     singleOf(::OauthNordigenApi)
     single<BankoDatabase> { CreateDatabase(get()).getDatabase() }
-    singleOf(::BankoDatabaseRepository)
+    singleOf(::TransactionsRepository)
+    singleOf(::ExpenseTagRepository)
+
+    // View models
+    viewModelOf(::SettingsScreenViewModel)
 }
 
 //    Example 1
