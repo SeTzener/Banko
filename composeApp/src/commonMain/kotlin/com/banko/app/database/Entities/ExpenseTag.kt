209bd9f2f67f88a1faf1a7dce3803e640a27a4c4
setzener <gaetanovf@gmail.com>
2025-02-23 12:48:00 +0100

    Add entities translation to model and viceversa

--- composeApp/src/commonMain/kotlin/com/banko/app/database/Entities/ExpenseTag.kt
+++ composeApp/src/commonMain/kotlin/com/banko/app/database/Entities/ExpenseTag.kt
@@ -1,7 +1,10 @@
 package com.banko.app.database.Entities
 
+import androidx.compose.ui.graphics.Color
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+
+typealias ModelExpenseTag = com.banko.app.ui.models.ExpenseTag
 
 @Entity(tableName = "expense_tag")
 data class ExpenseTag (
@@ -10,4 +13,11 @@ data class ExpenseTag (
     val name: String,
     val color: Long,
     val aka: List<String>?
+)
+
+fun ExpenseTag.toModel() = ModelExpenseTag (
+    id = id,
+    name = name,
+    color = Color(color),
+    aka = aka ?: emptyList()
 )
