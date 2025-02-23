7ea556bd06c74ef69f82ed7c4d526367d2855b4a
setzener <gaetanovf@gmail.com>
2025-02-23 12:48:42 +0100

    Ui Changes to check if it works

--- composeApp/src/commonMain/kotlin/com/banko/app/ui/screens/settings/SettingsComponent.kt
+++ composeApp/src/commonMain/kotlin/com/banko/app/ui/screens/settings/SettingsComponent.kt
@@ -1,9 +1,9 @@
 package com.banko.app.ui.screens.settings
 
 import com.arkivanov.decompose.ComponentContext
+import org.koin.compose.viewmodel.koinViewModel
+
 
 class SettingsComponent(
     componentContext: ComponentContext,
-): ComponentContext by componentContext {
-    val viewModel = SettingsScreenViewModel()
-}
+): ComponentContext by componentContext
