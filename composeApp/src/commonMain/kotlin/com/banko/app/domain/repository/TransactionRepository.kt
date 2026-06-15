9a77d3a0dbb64fb0f86dc83c4519ff2bdd26075c
setzener <gaetanovf@gmail.com>
2026-06-16 00:05:41 +0200

    Create pure domain model for Transaction/ExpenseTag and TransactionRepository interface

---
+++ composeApp/src/commonMain/kotlin/com/banko/app/domain/repository/TransactionRepository.kt
@@ -0,0 +1,15 @@
+package com.banko.app.domain.repository
+
+import com.banko.app.api.utils.Result
+import com.banko.app.domain.model.Transaction
+import kotlinx.coroutines.flow.Flow
+import kotlinx.datetime.LocalDateTime
+
+interface TransactionRepository {
+    fun getTransactions(limit: Int): Flow<List<Transaction>>
+    fun getTransactionsForMonth(date: LocalDateTime): Flow<List<Transaction>>
+    suspend fun fetchAndStoreTransactions(pageNumber: Int, pageSize: Int): Result<Long>
+    suspend fun getStoredTransactionCount(): Long
+    suspend fun getOldestTransactions(): LocalDateTime
+    suspend fun deleteTransaction(transactionId: String)
+}
